0d9a6df6477f205c6eecdb0ecbc8b02852468ac5
rraavi <raghuveer.raavi@gmail.com>
2017-04-16 19:18:15 -0400

    version v2 - added LdaMallet

--- data_clean.py
+++ data_clean.py
@@ -1,18 +1,15 @@
 import os
-from nltk.corpus import stopwords, wordnet as wn
-from nltk.stem import WordNetLemmatizer
-from nltk import word_tokenize
 import spacy
 
+from nltk import word_tokenize
+from nltk.corpus import stopwords, wordnet as wn
+from nltk.stem import WordNetLemmatizer
 
-path = os.getcwd()+'\\Risk\\correct\\training'
+path = os.getcwd() + '/Risk/correct/training'
 
-# file_contents = ''
 stop_words = stopwords.words('english')
 nlp = spacy.load('en')
 
-
-# print(stop_words)
 
 def hypernym(word):
     sets = wn.synsets(word)
@@ -26,25 +23,23 @@ def hypernym(word):
 
 def multi_word_exp(file_contents):
     doc = nlp(file_contents)
-    # print(file_contents)
     # print(doc)
-    # entities = []
     if doc.ents:
         for entity in doc.ents:
             # print(entity.label_, entity.text)
             if ' ' in entity.text:
                 file_contents = file_contents.replace(entity.text, entity.text.replace(" ", ''))
-                # for ent in doc.ents:
-                #     entities.append(ent.text)
-                #     print(entities)
-                # for item in doc:
-                #     # print(item.text)
-                #     for entity in doc.ents:
-                #         if item.text in entity.text:
-                #             print(item.text, item.ent_iob, item.ent_type_)
-
-                # for item in doc.ents:
-                #     print(item.merge('_'))
+    #             for ent in doc.ents:
+    #                 entities.append(ent.text)
+    #                 print(entities)
+    #             for item in doc:
+    #                 # print(item.text)
+    #                 for entity in doc.ents:
+    #                     if item.text in entity.text:
+    #                         print(item.text, item.ent_iob, item.ent_type_)
+    #
+    #             for item in doc.ents:
+    #                 print(item.merge('_'))
     # for item in doc.ents:
     #     print(item.text, item.label_)
     return file_contents
@@ -78,9 +73,11 @@ def process(word):
 
 
 def execute():
+    print('Initiating data pre-processing...')
     word_bag = []
     for filename in os.listdir(path):
-        with open(path + '\\' + filename, 'r', encoding='utf-8') as f:
+        print('Current file: ', filename)
+        with open(path + '/' + filename, 'r', encoding='utf-8') as f:
             temp_word_bag = []
             file_contents = f.read().strip()
             file_contents = multi_word_exp(file_contents)
@@ -90,6 +87,7 @@ def execute():
                 if word:
                     temp_word_bag.append(word)
             word_bag.append(temp_word_bag)
+        print('Done!')
     # print(len(word_bag_1))
     # print(word_bag, len(word_bag))
     #
@@ -98,6 +96,7 @@ def execute():
     #     for word in word_bag:
     #         line += word + ','
     #     f.write(line[0:-1])
+    print('Done pre-processing!')
     return word_bag
 
 
--- test v1.py
+++ test v1.py
@@ -1,16 +1,52 @@
+import os
+
+from datetime import datetime
 from gensim import corpora, models
+
 from data_clean import execute
 
 
-def ldaModel(word_bag):
-    dictionary = corpora.Dictionary(word_bag)
+def build_corpus(_word_bag):
+    print('Building dictionary...')
+    _dictionary = corpora.Dictionary(_word_bag)
+    print('Done building dictionary!')
     # print(dictionary)
-    corpus = [dictionary.doc2bow(word) for word in word_bag]
+    print('Building corpus...')
+    _corpus = [_dictionary.doc2bow(word) for word in _word_bag]
+    print('Done building corpus!')
     # print(corpus)
-    lda_model = models.ldamodel.LdaModel(corpus, num_topics=100, id2word=dictionary, passes=20)
-    print(lda_model)
+    return _corpus, _dictionary
+
+
+def ldaModel(_corpus, _dictionary):
+    print('--------------------Training LDA Model--------------------')
+    lda_model = models.ldamodel.LdaModel(corpus=_corpus, num_topics=100, id2word=_dictionary, passes=20)
+    print(lda_model, '\n')
+    print('--------------------------Done!--------------------------')
+    _topics = lda_model.show_topics(num_topics=100, num_words=5, formatted=False)
+    return _topics
+
+
+def ldaMallet(_corpus, _dictionary):
+    """class gensim.models.wrappers.ldamallet.LdaMallet(mallet_path, _corpus=None, num_topics=100, alpha=50,
+    id2word=None, workers=4, prefix=None, optimize_interval=0, iterations=1000, topic_threshold=0.0) """
+    print('--------------------Training LDA Mallet Model--------------------')
+    model = models.wrappers.LdaMallet(os.getcwd() + '/mallet-2.0.8/bin/mallet', corpus=_corpus, id2word=_dictionary)
+    model.convert_input(corpus=_corpus)
+    print('----------------------------Done!----------------------------')
+    # print(model.corpus2mallet(_corpus,'data.mallet'))
+    print('document topics @', model.fdoctopics())
+    print('topic keys @', model.ftopickeys())
+    print('word weights @', model.fwordweights())
+    _topics = model.show_topics(num_topics=100, num_words=5, formatted=False)
+    return _topics
 
 
 if __name__ == "__main__":
+    dt = datetime.now()
     word_bag = execute()
-    ldaModel(word_bag)
+    corpus, dictionary = build_corpus(_word_bag=word_bag)
+    ldaModel(_corpus=corpus, _dictionary=dictionary)
+    ldaMallet(_corpus=corpus, _dictionary=dictionary)
+    dt1 = datetime.now()
+    print('Time taken :- ', (dt1 - dt).seconds, 'seconds')
